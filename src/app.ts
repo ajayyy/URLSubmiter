4f712eaddc1c72571cb56ad74c48bbdf87ad3e25
Ajay Ramachandran <dev@ajay.app>
2021-01-20 22:02:20 -0500

    Raise request limit

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ export function createServer(callback: () => void) {
     //setup CORS correctly
     app.use(corsMiddleware);
     app.use(loggerMiddleware);
-    app.use(express.json());
+    app.use(express.json({limit: "150mb"}));
 
     // Setup pretty JSON
     if (config.mode === "development") app.set('json spaces', 2);
